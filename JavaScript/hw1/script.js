d0239871aea2b25dcacf399b15b152f8f907b60e
tonyreactus <indino@ya.ru>
2022-11-13 17:12:54 +0300

    JS hw1 fix

--- JavaScript/hw1/script.js
+++ JavaScript/hw1/script.js
@@ -1,3 +1,6 @@
 "use strict";
 
+// Создайте html-страницу  с заголовком h1 «I like JavaScript!».
+// Создайте и подключите внешний js-файл в head с defer, корый отобразит сообщение «What programming language do you like?» при помощи alert.
+
 alert("What programming language do you like?");
